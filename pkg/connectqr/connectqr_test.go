bf291263af21fcdfa003c612f4a74ba52ed8fafb
ArtemNaronov <temick1995@yandex.ru>
2026-07-13 14:23:02 +0400

    first comit

--- pkg/connectqr/connectqr_test.go
+++ pkg/connectqr/connectqr_test.go
@@ -3,8 +3,8 @@ package connectqr
 import "testing"
 
 func TestEncode(t *testing.T) {
-	got := Encode("http://10.8.1.1:8080", "secret")
-	want := `{"server":"http://10.8.1.1:8080","token":"secret"}`
+	got := Encode("http://198.51.100.10:8080", "secret")
+	want := `{"server":"http://198.51.100.10:8080","token":"secret"}`
 	if got != want {
 		t.Fatalf("Encode() = %q, want %q", got, want)
 	}
